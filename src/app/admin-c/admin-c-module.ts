d969924e81af1ca5c919e54cd3652c16a628641d
Deiberlyn Rivas <deiberlynr2505@gmail.com>
2025-12-02 23:58:43 -0400

    Agregado módulo admin-c y configuración de Angular actualizada

---
+++ src/app/admin-c/admin-c-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminCRoutingModule } from './admin-c-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdminCRoutingModule
+  ]
+})
+export class AdminCModule { }
