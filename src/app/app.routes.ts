0d442f749bcf4cf175c666beef698327ee1badb0
miguel soto <sotoguerrero2005@gmail.com>
2025-12-20 17:06:35 -0400

    ****Qué se hizo?****se agrego el logo de la academia y modifique las rutas****Qué archivos se cambiaron y para que?****app.routing.ts para agregar la ruta del login****Qué archivos nuevos hay?***

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      // Ruta por defecto: que vaya al login
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  // Login
+  { path: 'login', component: LoginComponent },
+];
